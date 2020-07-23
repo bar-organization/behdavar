49f669bb7ded8d2f546201faaaaa91a42bdb51f1
reza_moein <rezamoeinpe@gmail.com>
2020-07-23 15:53:23 +0430

    Fix bug: change schema name

--- behdavar-database/src/main/java/com/bar/behdavardatabase/constant/common/BaseConstant.java
+++ behdavar-database/src/main/java/com/bar/behdavardatabase/constant/common/BaseConstant.java
@@ -2,7 +2,7 @@ package com.bar.behdavardatabase.constant.common;
 
 public abstract class BaseConstant {
 
-    public static final String SCHEMA = "POMEGRANATE";
+    public static final String SCHEMA = "BEHDAVAR";
     public static final String BASE_TABLE_PREFIX = "BST_";
 
     protected BaseConstant() {
