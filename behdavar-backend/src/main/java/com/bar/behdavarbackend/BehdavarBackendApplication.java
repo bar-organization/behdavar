cbfdf1c7dd644b116fc68e46b11accdd1a417db2
reza_moein <rezamoeinpe@gmail.com>
2020-07-25 01:02:29 +0430

    Fix problem of WebMvcConfigurerAdapter deprecation
    move lombok to behdavar-common module

--- behdavar-backend/src/main/java/com/bar/behdavarbackend/BehdavarBackendApplication.java
+++ behdavar-backend/src/main/java/com/bar/behdavarbackend/BehdavarBackendApplication.java
@@ -3,11 +3,10 @@ package com.bar.behdavarbackend;
 import com.bar.behdavardatabase.configuration.EnableBehdavarDatabase;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
@@ -15,7 +14,7 @@ import java.util.Locale;
 
 @EnableBehdavarDatabase
 @SpringBootApplication
-public class BehdavarBackendApplication extends WebMvcConfigurerAdapter {
+public class BehdavarBackendApplication implements WebMvcConfigurer {
 
     @Bean
     public LocaleResolver localeResolver() {
