3e59bb657da83946876e6049c184cd3c4badb38d
reza_moein <rezamoeinpe@gmail.com>
2020-08-01 00:04:06 +0430

    Add row detail for document table

--- behdavar-front/src/app/document/document-search/document-search.component.ts
+++ behdavar-front/src/app/document/document-search/document-search.component.ts
@@ -1,14 +1,19 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup} from "@angular/forms";
 import {DocumentSearchLang} from '../../model/lang';
-import {group} from "@angular/animations";
-import {MatTableDataSource} from "@angular/material/table";
-import {SelectionModel} from "@angular/cdk/collections";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-document-search',
   templateUrl: './document-search.component.html',
-  styleUrls: ['./document-search.component.css']
+  styleUrls: ['./document-search.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class DocumentSearchComponent implements OnInit {
   documentSearchFormParent: FormGroup;
@@ -17,13 +22,14 @@ export class DocumentSearchComponent implements OnInit {
   documentSearchFormGroup: FormGroup;
 
   documentSearchLang: DocumentSearchLang = new DocumentSearchLang();
-  showToolbar = false;
 
-  // TODO temporary must remove
-  displayedColumns: string[] = ['select', 'position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
-  selection = new SelectionModel<PeriodicElement>(true, []);
+  myColumns: string[] = ['id', 'name', 'desc'];
+  expendedDocument: DocumentModel | null;
 
+  documentList: DocumentModel[] = [
+    {id: 1, name: 'doc1', desc: 'THis is test doc'},
+    {id: 1, name: 'doc1', desc: 'THis is test doc'},
+    {id: 1, name: 'doc1', desc: 'THis is test doc'}]
 
   constructor(private fb: FormBuilder) {
     this.bankMachineSearchFormGroup = fb.group({
@@ -64,34 +70,10 @@ export class DocumentSearchComponent implements OnInit {
     console.log(this.documentSearchFormParent);
   }
 
-  onChecked(event: any) {
-    console.log(event);
-    this.showToolbar = event.checked;
-
-  }
-  /** The label for the checkbox on the passed row */
-  checkboxLabel(row?: PeriodicElement): string {
-    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
-  }
 }
 
-// TODO this is temporary must remove
-export interface PeriodicElement {
+export interface DocumentModel {
+  id: number;
   name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  desc: string;
 }
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
