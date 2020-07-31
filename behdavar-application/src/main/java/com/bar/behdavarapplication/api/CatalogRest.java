21e00710d84c36532b5aba16aeadd05295622584
Behnam Aliaskari <b.aliaskari@gmail.com>
2020-07-31 14:32:38 +0430

    # 1-minor change

--- behdavar-application/src/main/java/com/bar/behdavarapplication/api/CatalogRest.java
+++ behdavar-application/src/main/java/com/bar/behdavarapplication/api/CatalogRest.java
@@ -8,30 +8,35 @@ import com.bar.behdavardatabase.entity.CatalogEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
 @RestController
 @RequestMapping("/catalog")
+@Validated
 public class CatalogRest {
 
     @Autowired
     CatalogBusiness catalogBusiness;
 
     @PostMapping("/find-by-id")
-    public ResponseEntity<CatalogEntity> findById(@RequestBody Long id) {
+    public ResponseEntity<CatalogEntity> findById(@RequestBody @NotNull Long id) {
         return new ResponseEntity<>(catalogBusiness.findById(id), HttpStatus.OK);
     }
 
     @PostMapping("/save")
-    public ResponseEntity<Long> save(@RequestBody CatalogDto dto) {
+    public ResponseEntity<Long> save(@Valid @RequestBody CatalogDto dto) {
         return new ResponseEntity<>(catalogBusiness.save(dto), HttpStatus.OK);
     }
 
     @PostMapping("/update")
-    public ResponseEntity<Void> update(@RequestBody CatalogDto dto) {
+    public ResponseEntity<Void> update(@RequestBody @Valid CatalogDto dto) {
         catalogBusiness.update(dto);
         return new ResponseEntity(HttpStatus.OK);
     }
--- behdavar-backend/src/main/java/com/bar/behdavarbackend/dto/CatalogDto.java
+++ behdavar-backend/src/main/java/com/bar/behdavarbackend/dto/CatalogDto.java
@@ -3,11 +3,18 @@ package com.bar.behdavarbackend.dto;
 import com.bar.behdavarbackend.dto.common.BaseAuditorDto;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Data
 public class CatalogDto extends BaseAuditorDto<Long, Long> {
 
+    @NotBlank(message = "validation.error.not.blank")
     private String code;
+    @NotBlank(message = "validation.error.not.blank")
     private String englishTitle;
+    @NotBlank(message = "validation.error.not.blank")
     private String title;
+    @NotNull(message = "validation.error.not.null")
     private Boolean active;
 }
