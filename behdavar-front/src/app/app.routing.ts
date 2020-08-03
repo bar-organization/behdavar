b397477c2fc760635c4407a4e2c9a0ed2d66df8c
alishirvani <@abbas1991>
2020-08-03 08:55:53 +0430

    fixed bug ts

--- behdavar-front/src/app/app.routing.ts
+++ behdavar-front/src/app/app.routing.ts
@@ -3,9 +3,9 @@
 import {HomeComponent} from './home';
 import {AuthGuard} from './_helpers';
 import {GuarantorsComponent} from './guarantors';
-import {LoginComponent} from "./login";
-import {CustomerComponent} from "./customer";
-import {FollowingComponent} from "./following";
+import {LoginComponent} from './login';
+import {CustomerComponent} from './customer';
+import {FollowingComponent} from './following';
 
 const routes: Routes = [
   {path: '', component: HomeComponent, canActivate: [AuthGuard]},
