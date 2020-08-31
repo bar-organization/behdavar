a61dfa6f72aa5dd7b6b8761a170e2673149b911a
Behnam Aliaskari <b.aliaskari@gmail.com>
2020-08-31 22:01:36 +0430

    #Make generic BaseAuditorTransformer

--- behdavar-backend/src/main/java/com/bar/behdavarbackend/business/transformer/BaseAuditorTransformer.java
+++ behdavar-backend/src/main/java/com/bar/behdavarbackend/business/transformer/BaseAuditorTransformer.java
@@ -3,8 +3,10 @@ package com.bar.behdavarbackend.business.transformer;
 import com.bar.behdavarbackend.dto.common.BaseAuditorDto;
 import com.bar.behdavardatabase.common.BaseAuditorEntity;
 
+import java.io.Serializable;
+
 public class BaseAuditorTransformer {
-    public static <E extends BaseAuditorEntity<String, Long>, D extends BaseAuditorDto<String, Long>> void transformAuditingFields(E e, D d) {
+    public static <E extends BaseAuditorEntity<U, I>, D extends BaseAuditorDto<U, I>, U, I extends Serializable> void transformAuditingFields(E e, D d) {
         d.setCreatedBy(e.getCreatedBy());
         d.setCreatedDate(e.getCreatedDate());
         d.setLastModifiedBy(e.getLastModifiedBy());
