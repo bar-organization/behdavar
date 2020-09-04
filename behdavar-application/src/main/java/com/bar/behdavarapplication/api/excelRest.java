165156561fba1c8db2b037e7b75a5d6457b885e0
Behnam Aliaskari <b.aliaskari@gmail.com>
2020-09-04 18:00:56 +0430

    #excel reader rest and test added.

---
+++ behdavar-application/src/main/java/com/bar/behdavarapplication/api/excelRest.java
@@ -0,0 +1,30 @@
+package com.bar.behdavarapplication.api;
+
+import com.bar.behdavarbackend.business.api.ExcelReaderBusiness;
+import com.bar.behdavarbackend.dto.CartableDto;
+import com.bar.behdavarbackend.dto.UploadExcelDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/excel")
+@Validated
+public class excelRest {
+
+    @Autowired
+    ExcelReaderBusiness excelReaderBusiness;
+
+    @PostMapping("/upload")
+    public ResponseEntity<CartableDto> findById(@RequestBody @Valid UploadExcelDto dto) {
+        excelReaderBusiness.readAndSave(dto);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
